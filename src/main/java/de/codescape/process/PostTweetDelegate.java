94541498310794d8b31fbbf61eb994eb050d4c8b
Stefan Glase <stefan.glase@googlemail.com>
2012-03-30 15:45:44 +0200

    initial version

---
+++ src/main/java/de/codescape/process/PostTweetDelegate.java
@@ -0,0 +1,18 @@
+package de.codescape.process;
+
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.JavaDelegate;
+
+import javax.inject.Named;
+import java.util.logging.Logger;
+
+@Named
+public class PostTweetDelegate implements JavaDelegate {
+
+    private static final Logger log = Logger.getLogger(PostTweetDelegate.class.getName());
+
+    public void execute(DelegateExecution execution) throws Exception {
+        log.info("Sending new tweet: " + execution.getVariable("message").toString());
+    }
+
+}
---
+++ src/test/java/de/codescape/process/TwitterPosting.java
@@ -0,0 +1,11 @@
+package de.codescape.process;
+
+public class TwitterPosting {
+
+    /* process key that is used in the diagram file */
+    public static final String PROCESS_DEFINITION_KEY = "TwitterPosting";
+
+    /* path to the diagram file relative to the project classpath */
+    public static final String PROCESS_DEFINITION_FILE = "diagrams/twitter-posting.bpmn20.xml";
+
+}
